f1ebc0cb6a82e739542e1fd73800dde950919718
xnohat <xnohat@gmail.com>
2014-04-08 18:59:10 +0700

    Update bleed.go

--- bleed.go
+++ bleed.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	out, err := bleed.Heartbleed(os.Args[1], []byte("heartbleed.filippo.io"))
+	out, err := bleed.Heartbleed(os.Args[1], []byte("heartbleed.filippo.idasdasdsadasdsadsadso"))
 	if err == bleed.ErrPayloadNotFound {
 		log.Printf("%v - SAFE", os.Args[1])
 		os.Exit(1)
